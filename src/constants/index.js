62d4388397798336b6912f2c8c19c19b5005a7c7
Ira Fich <ira.fich@gmail.com>
2018-06-21 16:02:50 -0400

    found a .babelrc setting that lets us have our cake and eat it too for testing

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,2 @@
-// XXX we should be able to use this format here, but CommonJS doesn't like it. Nicer during development, as long as you don't run any tests.
-// export * from "./misc";
-// export * from "./tables";
-
-// XXX have to use this format for testing. TODO can we use one or the other depending on environment?
-import * as misc from "./misc";
-import * as tables from "./tables";
-
-module.exports = { ...misc, ...tables };
+export * from "./misc";
+export * from "./tables";
