1788e7893cace84fae3955f47273b05019339589
Ira Fich <ira.fich@gmail.com>
2018-06-21 14:54:00 -0400

    fix (kinda ugly) to crash when testing. CommonJS doesn't like "export * from"

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,9 @@
-export * from "./misc";
-export * from "./tables";
+// XXX we should be able to use this format here, but CommonJS doesn't like it. Nicer during development, as long as you don't run any tests.
+// export * from "./misc";
+// export * from "./tables";
+
+// XXX have to use this format for testing. TODO can we use one or the other depending on environment?
+import * as misc from "./misc";
+import * as tables from "./tables";
+
+module.exports = { ...misc, ...tables };
--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,11 @@
-export * from "./misc";
-export * from "./choosers";
-export * from "./cr_targets";
+// XXX we should be able to use this format here, but CommonJS doesn't like it. Nicer during development, as long as you don't run any tests.
+// export * from "./misc";
+// export * from "./choosers";
+// export * from "./cr_targets";
+
+// XXX have to use this format for testing. TODO can we use one or the other depending on environment?
+import * as misc from "./misc";
+import * as choosers from "./choosers";
+import * as targets from "./cr_targets";
+
+module.exports = { ...misc, ...choosers, ...targets };
