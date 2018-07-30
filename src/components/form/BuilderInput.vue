3edeabfff7a560f3ae4e9b04b59e74ef61eb8418
Ira Fich <ira.fich@gmail.com>
2018-07-30 17:32:02 -0400

    tweaks to BuilderInput

--- src/components/form/BuilderInput.vue
+++ src/components/form/BuilderInput.vue
@@ -4,7 +4,7 @@
       <builder-label :forInput="inputId" :label="label" :right="labelRight" slot-scope="props">
         <fragment v-if="isSelect">
           <!--suppress HtmlFormInputWithoutLabel -->
-          <select :name="name" :id="name" v-model="value">
+          <select :name="name" :id="inputId" v-model="value" v-on="props.events"> <!--XXX should we really be using v-model here? Or alternatively, can we use that and skip v-on?-->
             <option v-for="option in options" :value="option">{{getLabel(option)}}</option>
           </select>
         </fragment>
@@ -13,7 +13,8 @@
           <!--suppress HtmlFormInputWithoutLabel -->
           <input :type="type" :name="props.name" :id="inputId" v-on="props.events" :list="datalistId"/>
           <datalist v-if="showDatalist" :id="datalistId">
-            <option v-for="option in options">{{getLabel(option)}}</option>
+            <option v-for="option in options">{{getLabel(option)}}</option> <!--XXX have to use the option key rather than its value proper (an object), because the value shows up in the dropdown.-->
+            <!--TODO make sure the output is the actual value object, not the key-->
           </datalist>
         </fragment>
       </builder-label>
@@ -33,7 +34,6 @@ export default {
     label: { type: String, required: true },
     type: { type: String, default: "text" },
     default: { type: [Object, String], default: "" },
-    //value: { type: [Object, String] }, // this is for things like radios and checkboxes I guess?
     labelRight: { type: Boolean },
     options: { type: [Array, Object] },
     customLabel: { type: Function } // TODO probably remove
