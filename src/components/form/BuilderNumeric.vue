ca78080ca2e087488346ac4a57cd44ce6d0d21b1
Ira Fich <ira.fich@gmail.com>
2018-08-02 15:36:24 -0400

    add numeric input

---
+++ src/components/form/BuilderNumeric.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="form-control">
+    <builder-label :forInput="inputId" :label="label" :right="labelRight">
+      <!--suppress HtmlFormInputWithoutLabel -->
+      <input :name="name" :id="inputId"
+             type="number" :value="value"
+             :min="min" :max="max" :step="step"
+             @input="onInput"/>
+    </builder-label>
+  </div>
+</template>
+
+<script>
+import BuilderLabel from "./BuilderLabel";
+
+export default {
+  name: "BuilderNumeric",
+  props: {
+    name: { type: String, required: true },
+    label: { type: String, required: true },
+    labelRight: { type: Boolean },
+    value: { type: [Object, String, Boolean, Number] },
+    min: Number,
+    max: Number,
+    step: Number
+  },
+  computed: {
+    inputId() {
+      // TODO should really get namespaced somehow. Or at least add a hash?
+      return this.name;
+    }
+  },
+  components: { BuilderLabel },
+  methods: {
+    onInput(event) {
+      this.$emit("input", event.target.value);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+input[type="number"] {
+  width: 3em; // XXX extremely arbitrary
+}
+</style>
