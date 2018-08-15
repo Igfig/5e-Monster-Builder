d21abbccd6d813c1beeccb319570b045a54996df
Ira Fich <ira.fich@gmail.com>
2018-08-15 19:40:18 -0400

    note

--- src/components/ControlInfo.vue
+++ src/components/ControlInfo.vue
@@ -2,6 +2,7 @@
   <div class="form-control" tabindex="0" @focus="setCurrentInfo(info)">
     <!--XXX not sure about the tab index being 0... it would be nice if we could generate a good order somehow-->
     <!--FIXME the info doesn't appear correctly if you click on a focusable element nested inside this -->
+    <!--FIXME I really hate that this is yet another div that doesn't do anything on its own. Rather we put all the bits on the next element down. Maybe we can use a mixin somehow?-->
     <slot/>
   </div>
 </template>
