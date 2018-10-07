b3ca08bde330d5bb32a28b1fc1b4132d62ef52b3
Ira Fich <ira.fich@gmail.com>
2018-10-07 02:02:04 -0400

    clean up old comment

--- src/constants/stats.js
+++ src/constants/stats.js
@@ -2,7 +2,6 @@ import { OrderedDict, compare } from "../classes/ordered_dict";
 
 export const ABILITIES = new OrderedDict(
   {
-    // XXX somewhat tempted to make the keys lowercase actually, so that when we get the corresponding entries from the store they won't be monster.abilityScores.STR.score or whatever
     STR: { order: 0, label: "Str", text: "strength" },
     DEX: { order: 1, label: "Dex", text: "dexterity" },
     CON: { order: 2, label: "Con", text: "constitution" },
